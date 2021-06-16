6ed9779215c8ebf8711326f41ca400a4b53a8f4c
fgazzelloni <docksbox@pm.me>
2021-06-16 17:56:20 +0200

    updating

--- w24/w24_fishery.R
+++ w24/w24_fishery.R
@@ -1,4 +1,4 @@
-# TidyTuesday week 24 - 
+# TidyTuesday week 24 -
 
 # Data Source: Great Lakes Fish,	Great Lakes Database,	Detroit Free Press
 #database of all fish stocked from artificial propagation into the Great Lakes
@@ -8,6 +8,10 @@ library(tidytuesdayR)
 library(tidyverse)
 library(stringr)
 library(hrbrthemes)
+library(viridis)
+library(forcats)
+library(ggridges)
+library(patchwork)
 
 # load data ##############################
 tuesdata <- tidytuesdayR::tt_load(2021, week = 24)
@@ -44,7 +48,31 @@ my_df <- fishing%>%
 
 
 # plotting #################################
-final <- my_df%>%
+
+fig1 <- my_df %>%
+  filter(!str_detect(species,"and")) %>%
+  arrange(year)%>%
+  mutate(text = fct_reorder(species, year )) %>%
+  ggplot( aes(y=text, x=year,fill=species)) +
+  geom_density_ridges_gradient(scale = 2, rel_min_height = 0.01) +
+  scale_fill_viridis(discrete=TRUE) +
+  scale_x_continuous(breaks=seq(1867, 2015, 8))+
+  labs(title="All Fisheries from the Great Lakes Fishery Commission",
+       subtitle="from 1867 to 2015",
+       tag = "Fig1")+
+  xlab("") +
+  ylab("Assigned Probability (%)")+
+  theme_ft_rc()+
+  theme(axis.text.x = element_text(angle=90),
+    legend.position="none",
+    plot.title.position = "plot",
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    panel.spacing = unit(0.1, "lines"),
+    strip.text.x = element_text(size = 8))
+
+
+fig2 <- my_df%>%
   filter(!str_detect(species,"and")) %>%
   group_by(year,species)%>%
   summarize(total_number=sum(grand_total),total_production=sum(values))%>%
@@ -53,18 +81,60 @@ final <- my_df%>%
   filter(!total_number==0)%>%
   mutate(percent=total_production/total_number)%>%
   filter(!percent>1)%>%
-  arrange(year,percent)%>%#filter(species==c("Pacific Salmon","White Bass","Carp","Lake Trout"))%>%
+  arrange(year,percent)%>%
+  filter(str_detect(species,("Lake")))%>%
   ggplot(aes(x=year,y=reorder(species,-total_production),group=species,color=species))+
-  geom_line()+
-  geom_point(aes(size=percent),alpha=0.5)+
-  labs(title="Fisheries species total production by Year",subtitle="sized by percentage of production",
-       tag="fig1",
-       x="Time(Year)",y="Species",
-       color="Species",size="Percent",
-       caption="Viz @fgazzelloni, DataSource: TidyTuesday Week24 - Great Lakes Fish,Great Lakes Database,Detroit Free Press")+
-  scale_x_continuous(breaks=seq(1867, 2015, 8))+
+  geom_line(aes(size=total_production))+
+  labs(title="Lake Fisheries by Year",
+       subtitle="sized by total value of production",
+       tag="Fig2",
+       size="Total value of production in $",
+       x="",
+       y="Species"
+       #color="Species",#size="Percent",
+       #caption="Viz @fgazzelloni, DataSource: TidyTuesday Week24 - Great Lakes Fish,Great Lakes Database,Detroit Free Press"
+       )+
+ scale_color_viridis(discrete = TRUE)+
+  guides(color = FALSE)+
+   scale_x_continuous(breaks=seq(1867, 2015, 18))+
   theme_ft_rc()+
-  theme(axis.text.x = element_text(angle=90))
+  theme(axis.text.x = element_text(angle=0),
+        legend.position = "top",
+        panel.grid = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank())
+
+fig3 <- my_df%>%
+  filter(!str_detect(species,"and")) %>%
+  arrange(year)%>%
+  filter(str_detect(species,("Lake")))%>%
+  ggplot(aes(x=species, y=values, fill=species)) +
+  geom_boxplot() +
+  scale_fill_viridis(discrete = TRUE, alpha=0.6) +
+  geom_jitter(aes(colour=values), size=0.3, alpha=0.5) +
+  scale_colour_viridis_c()+
+  labs(tag = "Fig3",
+       x="",y="Values",
+       caption="Viz @fgazzelloni, DataSource: TidyTuesday Week24 - Great Lakes Fish,Great Lakes Database,Detroit Free Press")+
+  theme_ft_rc() +
+  theme(
+    legend.position="none",
+    panel.background = element_blank(),
+    panel.grid = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank())
+
+
+
+
+
+
+
+final <- fig1+(fig2/fig3)
+
+final
+
+
 
 
 
@@ -89,14 +159,14 @@ tidy_logo<-image_read("https://raw.githubusercontent.com/rfordatascience/tidytue
   image_resize("300x300")
 
 
-final_plot <- image_read("w24_fisheries.png")
+final_plot <- image_read("w24/w24_fisheries.png")
 
 attached_logo <- image_composite(final_plot, tidy_logo,
                                  operator="atop",
                                  gravity="northeast") # tell R where to put the logo
 
 
-image_write(attached_logo, path = "w24_fisheries.png",
+image_write(attached_logo, path = "w24/w24_fisheries.png",
             format = "png") # save final plot
 
 
