6d4f067aa391b41949432d8945c756f1e61dd99f
Federica Gazzelloni <federica@MacBook-Pro-di-iGaz.local>
2021-09-14 09:20:46 +0200

    week37

--- w37/w37_f1.R
+++ w37/w37_f1.R
@@ -45,6 +45,7 @@ rank_df_long <- rank_df %>%arrange(-`1`)%>% slice(1:10)%>%
   mutate(position=as.numeric(position),
          position=factor(position))
 
+summary_first_position <- rank_df_long%>%filter(name==c("Mercedes","Red Bull","Ferrari"))%>%filter(position==1)%>%summary()
 
 library(hrbrthemes)
 library(GGally)
@@ -61,8 +62,8 @@ colors2 <- c("grey70", "grey70", "grey70",
 
 plot <- rank_df_long%>%
   ggplot(aes(x=fct_reorder(position,-position),y=points,group=name,color=factor(name)))+
-  geom_point(aes(size=points))+
-  geom_line(size=1.5)+
+  geom_point(aes(size=points),shape = 21, fill = "white",  stroke = 5)+
+  geom_line(size=2)+
   scale_color_manual(values = colors2)+
   labs(y="Points",x="Positions",
        color="Top 10 Constructors",size="Points")+
@@ -70,7 +71,7 @@ plot <- rank_df_long%>%
   theme(text = element_text(family="Impact",color="grey90"),
         plot.margin = margin(1,1,1,1,unit = "pt"),
         panel.grid.major.y = element_line(size=2),
-        panel.background = element_rect(color="grey40",fill="grey40"),
+        panel.background = element_rect(color="grey20",fill="grey20"),
         plot.background = element_rect(color="grey20",fill="grey20"),
         legend.key = element_blank(),
         legend.background = element_blank(),
@@ -106,24 +107,24 @@ final_plot <-
   final_plot +
 
   annotate(geom = "text", label = "Lewis Hamilton\nNico Rosberg\n Valtteri Bottas \n- Mercedes",
-           x = 0.11, y = 0.87,colour = "#00D2BE",size = 4,family = "Impact") +
-  annotate(geom = "curve", x = 0.1, xend = 0.15, y = 0.87, yend = 0.84, colour = "#00D2BE", curvature = -.3, arrow = arrow(length = unit(2, "mm")),family = "Impact") +
+           x = 0.11, y = 0.85,colour = "#00D2BE",size = 4,family = "Impact") +
+  annotate(geom = "curve", x = 0.07, xend = 0.09, y = 0.85, yend = 0.76, colour = "#00D2BE", curvature = .3, arrow = arrow(length = unit(2, "mm")),family = "Impact",size=1.5) +
 
   annotate(geom = "text", label = "Sebastian Vettel\nMark Webber\nMax Verstappen - Red Bull",
-           x = 0.25, y = 0.55,colour = "#0600EF",size = 4,family = "Impact") +
-  annotate(geom = "curve", x = 0.25, xend = 0.28, y = 0.57, yend = 0.69, colour = "#0600EF", curvature = -.3, arrow = arrow(length = unit(2, "mm")),family = "Impact") +
+           x = 0.23, y = 0.49,colour = "#0600EF",size = 4.5,family = "Impact") +
+  annotate(geom = "curve", x = 0.25, xend = 0.28, y = 0.53, yend = 0.69, colour = "#0600EF", curvature = -.3, arrow = arrow(length = unit(2, "mm")),family = "Impact",size=1.5) +
 
   annotate(geom = "text", label = "Charles Leclerc\nFernando Alonso\n - Ferrari",
-           x = 0.1, y = 0.65,colour = "#C00000",size = 4,family = "Impact") +
-  annotate(geom = "curve", x = 0.1, xend = 0.12, y = 0.68, yend = 0.71, colour = "#C00000", curvature = -.3, arrow = arrow(length = unit(2, "mm")),family = "Impact") +
+           x = 0.1, y = 0.63,colour = "#C00000",size = 4,family = "Impact") +
+  annotate(geom = "curve", x = 0.09, xend = 0.10, y = 0.67, yend = 0.70, colour = "#C00000", curvature = -.3, arrow = arrow(length = unit(2, "mm")),family = "Impact",size=1.5) +
 
   annotate(geom = "text", label = "F1",x = 0.85, y = 0.88, colour = "red", size = 5,family = "Impact") +
 
-  annotate(geom = "text", label = "MERCEDS, RED BULL and FERRARI\n reached the higest level of points\n at being number ones",x = 0.7, y = 0.82, colour = "white", size = 7,family = "Impact")+
+  annotate(geom = "text", label = "MERCEDES, RED BULL and FERRARI\n reached the higest level of points\n at being number one\nwith an average of 418.8 points",x = 0.7, y = 0.81, colour = "white", size = 7,family = "Impact")+
 
 
-  annotate(geom = "text", label = "Lewis Hamilton - McLaren",x = 0.7, y = 0.55, colour = "#FF8700", size = 5,family = "Impact") +
-  annotate(geom = "curve", x = 0.7, xend = 0.64, y = 0.55, yend = 0.41, colour = "#FF7F00", curvature = -.3, arrow = arrow(length = unit(2, "mm")),family = "Impact")
+  annotate(geom = "text", label = "Lewis Hamilton - McLaren",x = 0.7, y = 0.53, colour = "#FF8700", size = 5,family = "Impact") +
+  annotate(geom = "curve", x = 0.68, xend = 0.64, y = 0.52, yend = 0.41, colour = "#FF7F00", curvature = -.3, arrow = arrow(length = unit(2, "mm")),family = "Impact",size=1.5)
 
 library(ggimage)
 library(magick)
@@ -138,13 +139,11 @@ final <- ggdraw() +
   draw_image(logo_f1_img, x = 0.1, y = 0.47,width = 0.12)
 
 
-final
-
 
 ## Save final plot ----
 
-ragg::agg_png(here::here("w37/formula1.png"),
-              res = 320, width = 16, height = 12, units = "in")
+ragg::agg_png(here::here("w37/formula1_second_version.png"),
+              res = 320, width = 16, height = 8, units = "in")
 final
 
 dev.off()
